0d5204b8e8bfffcbb356f7716d19b8da850f39d5
Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at>
2011-06-09 20:14:22 +0200

    libc: Change sysinfo fields to 32 bit integers.

--- tools/libc/src/libgloss/lemberg/ioconsts.h
+++ tools/libc/src/libgloss/lemberg/ioconsts.h
@@ -5,20 +5,20 @@
 #ifndef _IOCONSTS_H
 #define _IOCONSTS_H
 
-#define SYSINFO_REVISION         (*((const unsigned char *)-64))
-#define SYSINFO_FREQUENCY        (*((const unsigned char *)-60))
-#define SYSINFO_FEATURES         (*((const unsigned char *)-56))
-#define SYSINFO_ADDR_WIDTH       (*((const unsigned char *)-52))
+#define SYSINFO_REVISION         (*((const unsigned int *)-64))
+#define SYSINFO_FREQUENCY        (*((const unsigned int *)-60))
+#define SYSINFO_FEATURES         (*((const unsigned int *)-56))
+#define SYSINFO_ADDR_WIDTH       (*((const unsigned int *)-52))
 
-#define SYSINFO_SRAM_ADDR_WIDTH  (*((const unsigned char *)-48))
-#define SYSINFO_DRAM_ADDR_WIDTH  (*((const unsigned char *)-44))
-#define SYSINFO_FLASH_ADDR_WIDTH (*((const unsigned char *)-40))
-#define SYSINFO_IO_ADDR_WIDTH    (*((const unsigned char *)-36))
+#define SYSINFO_SRAM_ADDR_WIDTH  (*((const unsigned int *)-48))
+#define SYSINFO_DRAM_ADDR_WIDTH  (*((const unsigned int *)-44))
+#define SYSINFO_FLASH_ADDR_WIDTH (*((const unsigned int *)-40))
+#define SYSINFO_IO_ADDR_WIDTH    (*((const unsigned int *)-36))
 
-#define SYSINFO_PC_WIDTH         (*((const unsigned char *)-32))
-#define SYSINFO_DM_ADDR_WIDTH    (*((const unsigned char *)-28))
-#define SYSINFO_FA_ADDR_WIDTH    (*((const unsigned char *)-24))
-#define SYSINFO_STACK_WIDTH      (*((const unsigned char *)-20))
+#define SYSINFO_PC_WIDTH         (*((const unsigned int *)-32))
+#define SYSINFO_DM_ADDR_WIDTH    (*((const unsigned int *)-28))
+#define SYSINFO_FA_ADDR_WIDTH    (*((const unsigned int *)-24))
+#define SYSINFO_STACK_WIDTH      (*((const unsigned int *)-20))
 
 #define TIMER_CYCLES             (*((volatile unsigned long long *)-96))
 #define TIMER_NANOS              (*((volatile unsigned long long *)-88))
