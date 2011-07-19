385abb679e701a26af249b81e10f667a382ce59a
Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at>
2011-07-19 20:26:52 +0200

    build: Larger default timeout for execution on FPGA.

--- fpga_exec.sh
+++ fpga_exec.sh
@@ -7,7 +7,7 @@ if [ "$3" = "RAW" ]; then
 	cat $1
 else
     # filtered output with timeout
-	`dirname $0`/timeout3 -t 60 awk 'BEGIN { boot = 0 }
+	`dirname $0`/timeout3 -t 120 awk 'BEGIN { boot = 0 }
          /^BOOT$/ { boot = 1; next }
          /^EXIT [0-9a-fA-F]+$/ { exit strtonum("0x"$2) }
          { if (boot) print }' < $1
