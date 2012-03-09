d730ac868c0b2e6c6402b4396f8e4168a74f1388
Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at>
2012-03-09 15:07:25 +0100

    libll,tools: Better support for software floating-point.

--- tools/libll/libllsyms.s
+++ tools/libll/libllsyms.s
@@ -28,12 +28,7 @@ declare float @__addsf3(float %a, float %b) nounwind readnone
 declare float @__subsf3(float %a, float %b) nounwind readnone
 declare float @__mulsf3(float %a, float %b) nounwind readnone
 declare float @__divsf3(float %a, float %b) nounwind readnone
-
-declare double @__floatsidf(i32 %a)   nounwind readnone
-declare double @__floatunsidf(i32 %a) nounwind readnone
-declare i32 @__fixdfsi(double %a)    nounwind readnone
-declare i32 @__fixunsdfsi(double %a) nounwind readnone
-
+		
 declare i32 @__eqdf2(double %a, double %b) nounwind readnone
 declare i32 @__gedf2(double %a, double %b) nounwind readnone
 declare i32 @__gtdf2(double %a, double %b) nounwind readnone
@@ -41,3 +36,23 @@ declare i32 @__ledf2(double %a, double %b) nounwind readnone
 declare i32 @__ltdf2(double %a, double %b) nounwind readnone
 declare i32 @__nedf2(double %a, double %b) nounwind readnone
 declare i32 @__unorddf2(double %a, double %b) nounwind readnone
+
+declare i32 @__eqsf2(float %a, float %b) nounwind readnone
+declare i32 @__gesf2(float %a, float %b) nounwind readnone
+declare i32 @__gtsf2(float %a, float %b) nounwind readnone
+declare i32 @__lesf2(float %a, float %b) nounwind readnone
+declare i32 @__ltsf2(float %a, float %b) nounwind readnone
+declare i32 @__nesf2(float %a, float %b) nounwind readnone
+declare i32 @__unordsf2(float %a, float %b) nounwind readnone
+
+declare double @__floatsidf(i32 %a)   nounwind readnone
+declare double @__floatunsidf(i32 %a) nounwind readnone
+declare i32 @__fixdfsi(double %a)    nounwind readnone
+declare i32 @__fixunsdfsi(double %a) nounwind readnone
+
+declare float @__floatsisf(i32 %a)   nounwind readnone
+declare float @__floatunsisf(i32 %a) nounwind readnone
+declare i32 @__fixsfsi(float %a)    nounwind readnone
+
+declare double @__extendsfdf2(float %a) nounwind readnone
+declare float @__truncdfsf2(double %a) nounwind readnone		
