00436aecc6a35387dcb13e618f7a945055b170f7
Wolfgang Puffitsch <hausen@gmx.at>
2013-01-03 20:23:48 +0100

    llvm: Improve delay slot filling code, use branch probabilities.

--- tools/llvm/lib/Target/Lemberg/LembergDelaySlotFiller.cpp
+++ tools/llvm/lib/Target/Lemberg/LembergDelaySlotFiller.cpp
@@ -17,6 +17,7 @@
 #include "LembergTargetMachine.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineBranchProbabilityInfo.h"
 #include "llvm/Target/TargetInstrInfo.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Support/Debug.h"
@@ -46,7 +47,9 @@ namespace {
       return "Lemberg Delay Slot Filler";
     }
 
-	  void fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &MBB, bool recurse);
+	virtual void getAnalysisUsage(AnalysisUsage &AU) const;
+
+	void fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &MBB, bool recurse, bool split);
 
     bool runOnMachineBasicBlock(MachineBasicBlock &MBB);
     bool runOnMachineFunction(MachineFunction &F) {
@@ -61,12 +64,19 @@ namespace {
   char Filler::ID = 0;
 } // end of anonymous namespace
 
-void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &MBB, bool recurse) {
+void Filler::getAnalysisUsage(AnalysisUsage &AU) const {
+    AU.addRequired<MachineBranchProbabilityInfo>();
+    MachineFunctionPass::getAnalysisUsage(AU);
+}
+
+void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &MBB, bool recurse, bool split) {
 
 	const LembergSubtarget *LST = ((LembergTargetMachine &)TM).getSubtargetImpl();
 	const InstrItineraryData &IID = LST->getInstrItins();
 	const InstrItinerary *IITab = IID.Itineraries;
 	const InstrStage *IIStages = IID.Stages;
+
+	const MachineBranchProbabilityInfo *MBPI = &getAnalysis<MachineBranchProbabilityInfo>();
 		
 	DebugLoc DL = II->getDebugLoc();
 
@@ -77,8 +87,8 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 	unsigned Opcode = II->getOpcode();
 
 	unsigned maxSlots = LembergSubtarget::DelaySlots;
-	if (Opcode == Lemberg::CALL
-		|| Opcode == Lemberg::CALLga
+	if (Opcode == Lemberg::CALLga
+		|| Opcode == Lemberg::CALL
 		|| Opcode == Lemberg::RET) {
 		maxSlots = LembergSubtarget::DelaySlots+1;
 	}
@@ -101,9 +111,12 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 			|| Opcode == Lemberg::CALLga
 			|| Opcode == Lemberg::RET)) {
 
+	    unsigned targReg = Opcode != Lemberg::CALL ? 0 : II->getOperand(2).getReg();
+
 		unsigned condReg = 0;
 		switch (Opcode) {
 		case Lemberg::JUMPpred:
+		case Lemberg::CALL:
 		case Lemberg::RET:					
 			condReg = II->getOperand(1).getReg();
 			break;
@@ -124,12 +137,15 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 
 		MachineBasicBlock::iterator J = prior(II), newII = II;				
 		unsigned movedSlots = 0;
+		bool conflictsLoad = false;
 		for (unsigned i = 0; i <= maxSlots; ++i) {
 			// look for a bundle with space in it
 			unsigned bundleSize = 0;
 			bool conflicts = false;
-			bool conflictsLate = false;
 			bool conflictsHere = false;
+			bool conflictsSplit = !split;
+			bool loadUse = false;
+			bool loadDef = false;			
 			--J;
 			while (J->getOpcode() != Lemberg::SEP) {
 						
@@ -141,6 +157,10 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 				if (condReg && J->definesRegister(condReg)) {							
 					conflicts = true;
 				}
+				// do not move past target definition
+				if (targReg && J->definesRegister(targReg)) {
+					conflicts = true;
+				}
 				// avoid conflicts with other delay slots
 				if (J->getDesc().hasDelaySlot()) {
 					unsigned ConflictOpcode = J->getOpcode();
@@ -148,24 +168,44 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 					if (recurse) {
 
 						if (Opcode == Lemberg::JUMP
-							&& !(II->getFlag((MachineInstr::MIFlag)(1 << 7)))
-							&& (ConflictOpcode == Lemberg::JUMPeqz
-								|| ConflictOpcode == Lemberg::JUMPeqz_now
-								|| ConflictOpcode == Lemberg::JUMPnez
-								|| ConflictOpcode == Lemberg::JUMPnez_now
-								|| ConflictOpcode == Lemberg::JUMPltz
-								|| ConflictOpcode == Lemberg::JUMPltz_now
-								|| ConflictOpcode == Lemberg::JUMPgez
-								|| ConflictOpcode == Lemberg::JUMPgez_now
-								|| ConflictOpcode == Lemberg::JUMPgtz
-								|| ConflictOpcode == Lemberg::JUMPgtz_now
-								|| ConflictOpcode == Lemberg::JUMPlez
-								|| ConflictOpcode == Lemberg::JUMPlez_now)) {
+							&& ((!(II->getFlag((MachineInstr::MIFlag)(1 << 7)))
+								 && (ConflictOpcode == Lemberg::JUMPeqz
+									 || ConflictOpcode == Lemberg::JUMPeqz_now
+									 || ConflictOpcode == Lemberg::JUMPnez
+									 || ConflictOpcode == Lemberg::JUMPnez_now
+									 || ConflictOpcode == Lemberg::JUMPltz
+									 || ConflictOpcode == Lemberg::JUMPltz_now
+									 || ConflictOpcode == Lemberg::JUMPgez
+									 || ConflictOpcode == Lemberg::JUMPgez_now
+									 || ConflictOpcode == Lemberg::JUMPgtz
+									 || ConflictOpcode == Lemberg::JUMPgtz_now
+									 || ConflictOpcode == Lemberg::JUMPlez
+									 || ConflictOpcode == Lemberg::JUMPlez_now))
+								|| ConflictOpcode == Lemberg::JUMPtrue
+								|| ConflictOpcode == Lemberg::JUMPtrue_now
+								|| ConflictOpcode == Lemberg::JUMPfalse
+								|| ConflictOpcode == Lemberg::JUMPfalse_now
+								|| ConflictOpcode == Lemberg::JUMPpred
+								|| ConflictOpcode == Lemberg::JUMPpred_now)) {
 
+						    const BranchProbability &Prob =
+							  MBPI->getEdgeProbability(&MBB, II->getOperand(0).getMBB());
+
+							MachineBasicBlock *ConfTarget;
+							if (ConflictOpcode == Lemberg::JUMPpred
+								|| ConflictOpcode == Lemberg::JUMPpred_now) {
+							  ConfTarget = J->getOperand(2).getMBB();
+							} else {
+							  ConfTarget = J->getOperand(1).getMBB();
+							}
+
+							const BranchProbability &ConfProb =
+							  MBPI->getEdgeProbability(&MBB, ConfTarget);
+;
 							MachineInstr *MI = J;
-							if (ConflictOpcode != Lemberg::JUMPeqz) {
+							if (ConfProb > Prob) {
 								// Move up conflicting branch first
-								fillDelaySlot(J, MBB, false);
+							    fillDelaySlot(J, MBB, false, true);
 							} else {
 								// Remove so we can schedule across this branch
 								MBB.erase(next(J)); // remove SEP
@@ -202,6 +242,18 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 							case Lemberg::JUMPlez_now:
 							  ConvOpcode = Lemberg::JUMPgtz;
 							  break;
+							case Lemberg::JUMPtrue:
+							case Lemberg::JUMPtrue_now:
+							  ConvOpcode = Lemberg::JUMPfalse;
+							  break;
+							case Lemberg::JUMPfalse:
+							case Lemberg::JUMPfalse_now:
+							  ConvOpcode = Lemberg::JUMPtrue;
+							  break;
+							case Lemberg::JUMPpred:
+							case Lemberg::JUMPpred_now:
+							  ConvOpcode = Lemberg::JUMPpred;
+							  break;
 							}
 
 							MCInstrDesc *NMID = new MCInstrDesc();
@@ -209,69 +261,49 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 							NMID->SchedClass = J->getDesc().getSchedClass();
 							II->setDesc(*NMID);
 							
-							II->addOperand(J->getOperand(0));
-							II->addOperand(Dest);
+							if (ConflictOpcode == Lemberg::JUMPpred
+								|| ConflictOpcode == Lemberg::JUMPpred_now) {
+							  int Cond = J->getOperand(0).getImm() == 0 ? 1 : 0;
+							  II->addOperand(MachineOperand::CreateImm(Cond));
+							  II->addOperand(J->getOperand(1));
+							  II->addOperand(Dest);
+							} else {
+							  II->addOperand(J->getOperand(0));
+							  II->addOperand(Dest);
+							}
 							
-							fillDelaySlot(II, MBB, false);
-
-							if (ConflictOpcode == Lemberg::JUMPeqz) {
+							if (ConfProb > Prob) {
+							    // Handle this branch
+							    fillDelaySlot(II, MBB, false, false);
+								if (ConflictOpcode == Lemberg::JUMPtrue
+									|| ConflictOpcode == Lemberg::JUMPtrue_now
+									|| ConflictOpcode == Lemberg::JUMPfalse
+									|| ConflictOpcode == Lemberg::JUMPfalse_now
+									|| ConflictOpcode == Lemberg::JUMPpred
+									|| ConflictOpcode == Lemberg::JUMPpred_now) {
+								  if (next(II) != J) II = J;
+								} else {
+								  // We already scheduled J, don't look at it again
+								  II = J;
+								}
+							} else {
+							    // Handle other branch
+							    fillDelaySlot(II, MBB, false, true);
 								// Insert this branch again
 								MBB.push_back(MI);
 								BuildMI(&MBB, DebugLoc(), TII->get(Lemberg::SEP));
 								// Move up branch
-								fillDelaySlot(J, MBB, false);
-							} else {
-								// We already scheduled J, don't look at it again
-								II = J;
+								fillDelaySlot(J, MBB, false, false);
+
+								if (ConflictOpcode == Lemberg::JUMPtrue
+									|| ConflictOpcode == Lemberg::JUMPtrue_now
+									|| ConflictOpcode == Lemberg::JUMPfalse
+									|| ConflictOpcode == Lemberg::JUMPfalse_now
+									|| ConflictOpcode == Lemberg::JUMPpred
+									|| ConflictOpcode == Lemberg::JUMPpred_now) {
+								  if (next(J) == II) II = J;
+								}
 							}
-
-							return;
-						} else if (Opcode == Lemberg::JUMP
-							&& (ConflictOpcode == Lemberg::JUMPtrue
-								|| ConflictOpcode == Lemberg::JUMPtrue_now
-								|| ConflictOpcode == Lemberg::JUMPfalse
-								|| ConflictOpcode == Lemberg::JUMPfalse_now)) {
-
-							// Move up conflicting branch first
-							fillDelaySlot(J, MBB, false);
-							// Change to reversed branch and move up
-							MachineOperand Dest = II->getOperand(0);
-							II->RemoveOperand(0);
-							
-							unsigned ConvOpcode = 0;
-							if (ConflictOpcode == Lemberg::JUMPtrue
-								|| ConflictOpcode == Lemberg::JUMPtrue_now) {
-								ConvOpcode = Lemberg::JUMPfalse;
-							} else {
-								ConvOpcode = Lemberg::JUMPtrue;
-							}
-							II->setDesc(TII->get(ConvOpcode));
-							II->addOperand(J->getOperand(0));
-							II->addOperand(Dest);
-							
-							fillDelaySlot(II, MBB, false);
-							// Continue after the one that's scheduled highest
-							if (next(II) != J) II = J;
-							return;
-						} else if (Opcode == Lemberg::JUMP
-								   && (ConflictOpcode == Lemberg::JUMPpred
-									   || ConflictOpcode == Lemberg::JUMPpred_now)) {
-
-							// Move up conflicting branch first
-							fillDelaySlot(J, MBB, false);
-							// Change to reversed branch and move up
-							MachineOperand Dest = II->getOperand(0);
-							II->RemoveOperand(0);
-									
-							II->setDesc(TII->get(Lemberg::JUMPpred));							
-							int Cond = J->getOperand(0).getImm() == 0 ? 1 : 0;
-							II->addOperand(MachineOperand::CreateImm(Cond));
-							II->addOperand(J->getOperand(1));
-							II->addOperand(Dest);
-							
-							fillDelaySlot(II, MBB, false);
-							// Continue after the one that's scheduled highest
-							if (next(II) != J) II = J;
 							return;
 						} else {
 							conflicts = true;
@@ -280,13 +312,16 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 						if ((Opcode == Lemberg::JUMPpred 
 							 && (ConflictOpcode == Lemberg::JUMPpred
 								 || ConflictOpcode == Lemberg::JUMPpred_now))
-							|| (Opcode == Lemberg::JUMPtrue
+							|| ((Opcode == Lemberg::JUMPtrue
+								 || Opcode == Lemberg::JUMPtrue_now)
 								&& (ConflictOpcode == Lemberg::JUMPfalse
 									|| ConflictOpcode == Lemberg::JUMPfalse_now))
-							|| (Opcode == Lemberg::JUMPfalse
+							|| ((Opcode == Lemberg::JUMPfalse
+								 || Opcode == Lemberg::JUMPfalse_now)
 								&& (ConflictOpcode == Lemberg::JUMPtrue
 									|| ConflictOpcode == Lemberg::JUMPtrue_now))) {
-							/* no conflict in these cases */
+							/* no real conflict in these cases, but don't split */
+						    conflictsSplit = true;
 						} else {
 							conflicts = true;
 						}
@@ -298,7 +333,7 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 					conflicts = true;
 				}
 				// $rb and $ro must be saved before calls
-				if (Opcode == Lemberg::CALLga) {
+				if (Opcode == Lemberg::CALLga || Opcode == Lemberg::CALL) {
 					if (J->getOpcode() == Lemberg::MOVExa
 						&& (J->getOperand(3).getReg() == Lemberg::RB
 							|| J->getOperand(3).getReg() == Lemberg::RO)) {
@@ -314,10 +349,15 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 					}
 				}						
 
-				if (Opcode == Lemberg::CALLga || Opcode == Lemberg::RET) {
-					// call/return must not be moved across loads of memory results
+				if (Opcode == Lemberg::CALLga || Opcode == Lemberg::CALL 
+					|| Opcode == Lemberg::RET) {
+					// call/return must not be moved into loads of memory results
 				    if (J->readsRegister(Lemberg::R31)) {
-						conflictsLate = true;
+					    loadUse = true;
+						conflictsSplit = true;
+					}
+				    if (J->definesRegister(Lemberg::R31)) {
+					    loadDef = true;
 					}
 					// call/return needs the memunit
 					if (J->getDesc().mayLoad() || J->getDesc().mayStore()) {
@@ -346,12 +386,17 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 				--J;
 			}
 
+			// Load ended here and no new load started
+			if (loadDef && !loadUse) {
+			  conflictsLoad = false;
+			}
+
 			if (conflicts) {
 				break;
 			}
 
 			// okay, we found some room for the jump
-			if (bundleSize < ClusterCount && !conflictsHere && J != II) {
+			if (bundleSize < ClusterCount && !conflictsHere && !conflictsLoad && J != II) {
 				// remove what would be consecutive separators
 				if (next(II)->getOpcode() == Lemberg::SEP
 					&& prior(II)->getOpcode() == Lemberg::SEP) {
@@ -360,9 +405,32 @@ void Filler::fillDelaySlot(MachineBasicBlock::iterator &II, MachineBasicBlock &M
 				MBB.insertAfter(J, II->removeFromParent());
 				newII = next(J);
 				movedSlots = i;
+			} else if (i < maxSlots && !conflictsLoad && !conflictsSplit && J != II) {
+			    // insert as new bundle
+				if (next(II)->getOpcode() == Lemberg::SEP
+					&& prior(II)->getOpcode() == Lemberg::SEP) {
+					prior(II)->eraseFromParent();
+				}
+				if (J->getOpcode() == Lemberg::SEP) {
+				  BuildMI(MBB, J, DebugLoc(), TII->get(Lemberg::SEP));
+				  MBB.insert(J, II->removeFromParent());
+				  newII = prior(J);
+				  J = prior(newII);
+				} else {
+				  MBB.insert(J, II->removeFromParent());
+				  BuildMI(MBB, J, DebugLoc(), TII->get(Lemberg::SEP));
+				  newII = prior(prior(J));
+				  J = newII;
+				}
+				movedSlots = i+1;
 			}
 
-			if (J == MBB.begin() || conflictsLate) {
+			// Load started here
+			if (loadUse) {
+			  conflictsLoad = true;
+			}
+
+			if (J == MBB.begin()) {
 				break;
 			}
 		}
@@ -451,9 +519,9 @@ runOnMachineBasicBlock(MachineBasicBlock &MBB)
 {
 	bool Changed = false;
 	for (MachineBasicBlock::iterator II = MBB.end(); II != MBB.begin(); ) {
-		--II;		
+		--II;
 		if (II->getDesc().hasDelaySlot()) {
-			fillDelaySlot(II, MBB, true);
+		    fillDelaySlot(II, MBB, true, true);
 			Changed = true;
 		}
 	}
