794a98f4a1af9abdc4bbabaa474daa6743647494
Wolfgang Puffitsch <hausen@gmx.at>
2013-01-03 20:07:55 +0100

    hw: Rewrite flags to do muxing on reading instead of writing.

--- hw/src/flags.vhd
+++ hw/src/flags.vhd
@@ -23,9 +23,9 @@ use work.flag_pack.all;
 
 entity flags is
 	port (
-		clk    : in  std_logic;
-		reset  : in  std_logic;
-		ena    : in  std_logic;
+		clk	   : in	 std_logic;
+		reset  : in	 std_logic;
+		ena	   : in	 std_logic;
 		rddata : out std_logic_vector(FLAG_COUNT-1 downto 0);
 		wren   : in	 flag_wren_type;
 		wrdata : in	 flag_wrdata_type);
@@ -33,28 +33,40 @@ end flags;
 
 architecture behavior of flags is
 
-	signal flags : std_logic_vector(FLAG_COUNT-1 downto 0);
+	signal flags : flag_wrdata_type;
+
+	type src_type is array (1 to FLAG_COUNT-1) of integer range 0 to CLUSTERS-1;
+	signal src : src_type;
 	
 begin  -- behavior
 
-	rddata <= flags;
-
 	sync: process (clk, reset)
+		variable en, fl : std_logic_vector(FLAG_COUNT-1 downto 1);
 	begin  -- process sync
-		if reset = '0' then  			-- asynchronous reset (active low)
-			flags <= (others => '0');
-			flags(0) <= '1';  			-- flag zero is always true
-		elsif clk'event and clk = '1' then  -- rising clock edge
-			for i in 0 to CLUSTERS-1 loop
-				for k in 1 to FLAG_COUNT-1 loop
-					if wren(i)(k) = '1' and ena = '1' then
-						flags(k) <= wrdata(i)(k);
-					end if;
-				end loop;  -- k
+		if reset = '0' then				-- asynchronous reset (active low)
+			flags <= (others => (others => '0'));
+			for i in 1 to FLAG_COUNT-1 loop
+				src(i) <= 0;
 			end loop;  -- i
-			-- condition flag zero is read-only and always true
-			flags(0) <= '1';
+		elsif clk'event and clk = '1' then	-- rising clock edge
+			for k in 1 to FLAG_COUNT-1 loop
+				for i in 0 to CLUSTERS-1 loop
+					if wren(i)(k) = '1' and ena = '1' then
+						src(k) <= i;
+						flags(i)(k) <= wrdata(i)(k);
+					end if;
+				end loop;  -- i
+			end loop;  -- k			   
 		end if;
 	end process sync;
 
+	async: process (flags, src)
+	begin  -- process async
+		for k in 1 to FLAG_COUNT-1 loop
+			rddata(k) <= flags(src(k))(k);
+		end loop;  -- k
+		-- condition flag zero is read-only and always true
+		rddata(0) <= '1';	 
+	end process async;
+	
 end behavior;
